a8b0dd740578d173f4a944a055405b06765dc728
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-09 17:33:15 -0400

    Updated color scheme

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,17 +11,16 @@ export default function Character(props) {
         <div className="character-profile-container">
 
             <div className="photo-and-name">
-                <h2>Name: {props.chars.name}</h2>
+                <h2><span class="label">Name:</span> {props.chars.name}</h2>
                 <img src={props.chars.image} alt="rick and morty" />
             </div>
             
             
 
             <div className="vitals">
-                <h3>Status: {props.chars.status}</h3>
-                <h3>Name: {props.chars.name}</h3>
-                <h3>Species: {props.chars.species}</h3>
-                <h3>Last known location: {props.chars.location.name}</h3>
+                <h3><span class="label">Status:</span> {props.chars.status}</h3>
+                <h3><span class="label">Species:</span> {props.chars.species}</h3>
+                <h3><span class="label">Last known location:</span> {props.chars.location.name}</h3>
 
             </div>
 
