70357637437d2a074bf008feb0e7319a1190e3c5
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-09 17:15:38 -0400

    Styling changes - rounded photo borders

--- src/components/Character.js
+++ src/components/Character.js
@@ -8,11 +8,23 @@ export default function Character(props) {
     const { chars } = props;
     
     return(
-        <div className="character-profile">
-            <h2>Name: {props.chars.name}</h2>
-            <img src={props.chars.image} alt="rick and morty" />
-            <h3>Status: {props.chars.status}</h3>
-        </div>
-        
+        <div className="character-profile-container">
+
+            <div className="photo-and-name">
+                <h2>Name: {props.chars.name}</h2>
+                <img src={props.chars.image} alt="rick and morty" />
+            </div>
+            
+            
+
+            <div className="vitals">
+                <h3>Status: {props.chars.status}</h3>
+                <h3>Name: {props.chars.name}</h3>
+                <h3>Species: {props.chars.species}</h3>
+                <h3>Last known location: {props.chars.location.name}</h3>
+
+            </div>
+
+            </div>
     )
 }
